8d6603c6ee0efa5a6aee3ca267695ae807853db1
Vaibhav5027 <patil.vaibhav5027@gmail.com>
2024-03-02 22:16:56 +0530

    ecommerce using rest

---
+++ ecommerce/src/test/java/com/vaibs/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibs.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
