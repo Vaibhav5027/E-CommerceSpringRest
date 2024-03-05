ab1505635925897fb21174c6f31a8fa82017a071
Vaibhav5027 <patil.vaibhav5027@gmail.com>
2024-03-05 09:39:27 +0530

    method added in repositoy to find product by category id

--- ecommerce/src/main/java/com/infy/ecommerce/dao/ProductRepository.java
+++ ecommerce/src/main/java/com/infy/ecommerce/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.infy.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -8,4 +11,6 @@ import com.infy.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200/")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	Page<Product> findByCategoryId(@Param("id") int categoryId,Pageable peagble);
 }
