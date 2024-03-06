97f8a6236ec4ee571086815d09375c9dde62a655
Vaibhav5027 <patil.vaibhav5027@gmail.com>
2024-03-06 09:12:21 +0530

    product by category

--- ecommerce-Frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-Frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,31 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from '../common/product';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Product } from "../common/product";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { ProductCategory } from "../common/product-category";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
+  private baseUrl = "http://localhost:8080/api/products";
+  private categoryUrl = "http://localhost:8080/api/product-category";
 
-  private baseUrl = 'http://localhost:8080/api/products';
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-
-    // need to build URL based on category id 
+    // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.httpClient
+      .get<GetResponse>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+
+  getCategoryList(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetCategoryResponse>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 }
 
 interface GetResponse {
   _embedded: {
     products: Product[];
-  }
+  };
+}
+interface GetCategoryResponse {
+  _embedded: {
+    productCategory: ProductCategory[];
+  };
 }
